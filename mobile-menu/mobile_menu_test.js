4b770daebcc62ef5200de4f3d24e1605c82e0c6c
Phillips <rcphilli@illinois.edu>
2017-03-08 10:24:56 -0600

    Add menu shadow

--- mobile-menu/mobile_menu_test.js
+++ mobile-menu/mobile_menu_test.js
@@ -4,14 +4,12 @@ var openMenu=function() {
 	moving=document.getElementsByClassName("mobile-menu")[0];
 	moving.style.right="0%";
 	document.getElementById("open-menu").onclick=null;
-	document.getElementById("body").style.left="-70%";
 }
 
 var closeMenu=function() {
 	moving=document.getElementsByClassName("mobile-menu")[0];
-	moving.style.right="-70%";
+	moving.style.right="-75%";
 	document.getElementById("open-menu").onclick=openMenu;
-	document.getElementById("body").style.left="0%";
 }
 
 var openSub=function(index) {
@@ -21,5 +19,5 @@ var openSub=function(index) {
 
 var closeSub=function(index) {
 	moving=document.getElementById("submenu-container").children[index].children[0];
-	moving.style.right="-70%";
+	moving.style.right="-75%";
 }
