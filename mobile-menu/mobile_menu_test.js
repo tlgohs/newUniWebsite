ba10e195027c128c43b7539f12eedc082b548855
Julian Montague <roast@roastchicken.io>
2017-05-04 08:03:58 -0700

    Fix whitespace Change whitespace to match rest of project, including: - Tabs to four spaces - Newlines at ends of files - Spaces around equals signs

--- mobile-menu/mobile_menu_test.js
+++ mobile-menu/mobile_menu_test.js
@@ -1,23 +1,23 @@
 var moving;
 
-var openMenu=function() {
-	moving=document.getElementsByClassName("mobile-menu")[0];
-	moving.style.right="0%";
-	document.getElementById("open-menu").onclick=null;
+var openMenu = function() {
+    moving = document.getElementsByClassName("mobile-menu")[0];
+    moving.style.right = "0%";
+    document.getElementById("open-menu").onclick = null;
 }
 
-var closeMenu=function() {
-	moving=document.getElementsByClassName("mobile-menu")[0];
-	moving.style.right="-75%";
-	document.getElementById("open-menu").onclick=openMenu;
+var closeMenu = function() {
+    moving = document.getElementsByClassName("mobile-menu")[0];
+    moving.style.right = "-75%";
+    document.getElementById("open-menu").onclick = openMenu;
 }
 
-var openSub=function(index) {
-	moving=document.getElementById("submenu-container").children[index].children[0];
-	moving.style.right="0%";
+var openSub = function(index) {
+    moving = document.getElementById("submenu-container").children[index].children[0];
+    moving.style.right = "0%";
 }
 
-var closeSub=function(index) {
-	moving=document.getElementById("submenu-container").children[index].children[0];
-	moving.style.right="-75%";
+var closeSub = function(index) {
+    moving = document.getElementById("submenu-container").children[index].children[0];
+    moving.style.right = "-75%";
 }
